a112c41474c838a09cfd028d1aa926a98486282d
David Trujillo <davot098@gmail.com>
2023-03-29 18:05:24 -0500

    Implementacion de copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     //punto de entrada
@@ -49,5 +50,15 @@ module.exports = {
             filename: './index.html'
         }),
         new MiniCssExtractPlugin(),
+        new CopyPlugin(
+            {
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, "src", "assets/images"),
+                        to: "assets/images"
+                    }
+                ]
+            }
+        ),
     ]
 }

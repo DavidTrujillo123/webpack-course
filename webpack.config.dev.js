161fccbd0a802c73f7f76ce9acf8c8360e348618
David Trujillo <davot098@gmail.com>
2023-03-29 22:08:03 -0500

    Implementacion de alias y variables de entorno

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,8 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('terser-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
@@ -17,8 +15,8 @@ module.exports = {
         (__dirname, 'dist'),
         //Archivo resultante
         filename: '[name].[contenthash].js',
-        clean: true,
     } ,
+    mode: 'development',
     resolve:{
         //extenciones con las que vamos a trabajar
         //pueden variar con svelt, react
@@ -92,12 +90,4 @@ module.exports = {
         ),
         new Dotenv(),
     ],
-    //optimizacion
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin()
-        ]
-    }
 }
